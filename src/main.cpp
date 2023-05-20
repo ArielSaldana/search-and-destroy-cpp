89c84ed3ad2688f1bbd06357511d07c7b2d44146
Ariel Saldana <ariel@unreal.io>
2023-05-19 23:41:08 -0400

    Refactor search code

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,39 @@
-#include <string>
-#include <iostream>
-#include <chrono>
 #include "search.h"
+#include <chrono>
+#include <iostream>
+#include <string>
 
 int main() {
-    // Get the start time
-    auto start = std::chrono::high_resolution_clock::now();
+  // Get the start time
+  auto start = std::chrono::high_resolution_clock::now();
 
-    std::string root_directory = "/Users/ariel/Movies";
-    auto files_map = search::search_directory(root_directory);
+  const std::string root_directory = "/Users/ariel/Movies";
+  auto files_map = search::search_directory(root_directory);
 
-    size_t count = 0;
-    for (const auto &pair: files_map) {
-        if (pair.second.size() > 1) {
-            count++;
-        }
+  size_t count = 0;
+  for (const auto &pair : files_map) {
+    if (pair.second.size() > 1) {
+      count++;
     }
+  }
 
-    std::cout << "Total number of suspected dupes: " << count << std::endl;
+  std::cout << "Total number of suspected dupes: " << count << std::endl;
 
-    // Get the end time
-    auto end = std::chrono::high_resolution_clock::now();
-    // Compute the difference between the two times in microseconds
-    auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-    std::cout << "Elapsed time: " << elapsed.count() / 1000 << " ms" << std::endl;
-    std::cout << "Suspected dupes" << std::endl;
+  // Get the end time
+  auto end = std::chrono::high_resolution_clock::now();
+  // Compute the difference between the two times in microseconds
+  auto elapsed =
+      std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+  std::cout << "Elapsed time: " << elapsed.count() / 1000 << " ms" << std::endl;
+  std::cout << "Suspected dupes" << std::endl;
 
-    for (auto const &map_pair: files_map) {
-        if (map_pair.second.size() > 1) {
-            for (const auto &file: map_pair.second) {
-                std::cout << map_pair.first << " / " << file << std::endl;
-            }
-        }
+  for (auto const &map_pair : files_map) {
+    if (map_pair.second.size() > 1) {
+      for (const auto &file : map_pair.second) {
+        std::cout << map_pair.first << " / " << file << std::endl;
+      }
     }
+  }
 
-    return 0;
+  return 0;
 }
--- src/search.h
+++ src/search.h
@@ -5,23 +5,23 @@
 #ifndef SEARCH_AND_DESTROY_CPP_SEARCH_H
 #define SEARCH_AND_DESTROY_CPP_SEARCH_H
 
-#include <vector>
-#include <string>
 #include <filesystem>
-#include <unordered_map>
 #include <map>
+#include <string>
+#include <unordered_map>
+#include <vector>
 
 class search {
 private:
-    static std::vector<std::string> get_directory_files(const std::filesystem::path &path);
+  static std::vector<std::string>
+  get_directory_files(const std::filesystem::path &path);
 
-    static std::unordered_map<std::string, std::vector<std::string>>
-    get_file_hashes(const std::vector<std::string> &files_paths);
+  static std::unordered_map<std::string, std::vector<std::string>>
+  get_file_hashes(const std::vector<std::string> &files_paths);
 
 public:
-    static std::unordered_map<std::string, std::vector<std::string>>
-    search_directory(const std::string &root_directory);
+  static std::unordered_map<std::string, std::vector<std::string>>
+  search_directory(const std::string &root_directory);
 };
 
-
-#endif //SEARCH_AND_DESTROY_CPP_SEARCH_H
+#endif // SEARCH_AND_DESTROY_CPP_SEARCH_H
