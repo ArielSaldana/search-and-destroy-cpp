6a99a60947ecc9fa9971667c24cda925cfb8f7b4
Ariel Saldana <ariel@unreal.io>
2023-05-21 16:29:19 -0400

    apply code styles to header files

--- include/search.h
+++ include/search.h
@@ -14,22 +14,22 @@
 class search
 {
 
-  public:
-    [[nodiscard]] static std::vector<std::pair<std::string, std::string> >
-    search_directory (const std::string &root_directory);
+public:
+    [[nodiscard]] static std::vector<std::pair<std::string, std::string>> search_directory(
+        const std::string& root_directory);
 
-  private:
-    [[nodiscard]] bool are_files_same_size (const std::string &first_file, const std::string &second_file);
+private:
+    [[nodiscard]] bool are_files_same_size(const std::string& first_file, const std::string& second_file);
 
-    [[nodiscard]] static uint64_t file_size (const std::string &file_path);
+    [[nodiscard]] static uint64_t file_size(const std::string& file_path);
 
-    [[nodiscard]] static std::vector<std::string> get_directory_files (const std::filesystem::path &path);
+    [[nodiscard]] static std::vector<std::string> get_directory_files(const std::filesystem::path& path);
 
-    [[nodiscard]] static std::unordered_map<std::string, std::vector<std::string> >
-    get_file_hashes (const std::vector<std::string> &files_paths);
+    [[nodiscard]] static std::unordered_map<std::string, std::vector<std::string>> get_file_hashes(
+        const std::vector<std::string>& files_paths);
 
-    [[nodiscard]] static std::vector<std::pair<std::string, std::string> >
-    filter_different_files (std::unordered_map<std::string, std::vector<std::string> > &file_hashes);
+    [[nodiscard]] static std::vector<std::pair<std::string, std::string>> filter_different_files(
+        std::unordered_map<std::string, std::vector<std::string>>& file_hashes);
 };
 
 #endif // SEARCH_AND_DESTROY_CPP_SEARCH_H
