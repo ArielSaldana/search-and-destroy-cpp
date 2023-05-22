bad96522a2cf52f456ba9ff1e9b452d862b9dc58
Ariel Saldana <ariel@unreal.io>
2023-05-22 11:46:20 -0400

    update main to take in args, and another update for language.

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,18 @@
 #include <string>
 
 int
-main()
+main(int argc, char* argv[])
 {
     // Get the start time
     auto start = std::chrono::high_resolution_clock::now();
 
+    // convert the args into a vector of strings
+    std::vector<std::string> args(argv, argv + argc);
+
     // Run the search function to get the vector of pairs
     // <duplicate_file_to_delete, original_file>
-    const std::string root_directory = "/Users/ariel/Movies";
+    const std::string root_directory = args[1];
+    std::cout << "Root directory set to: " << root_directory << std::endl;
     auto files_to_delete = search::search_directory(root_directory);
 
     // Get the end time
@@ -21,7 +25,7 @@ main()
 
     // Print things
     std::cout << "Elapsed time: " << elapsed.count() << " ms" << std::endl;
-    std::cout << "Total number of suspected dupes: " << files_to_delete.size() << std::endl;
+    std::cout << "Total number of suspected duplicates: " << files_to_delete.size() << std::endl;
 
     // Attempting to destroy files
     auto destroy_result = destroy::destroy_files(files_to_delete);
